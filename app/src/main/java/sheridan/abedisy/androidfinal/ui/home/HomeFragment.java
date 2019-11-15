21c9359b8b05d23e4adbab62355a6abd00c06a73
fetchandreleash <crjoshua@sheridancollege.ca>
2019-11-15 01:36:32 -0500

    Final Good Commit.

--- app/src/main/java/sheridan/abedisy/androidfinal/ui/home/HomeFragment.java
+++ app/src/main/java/sheridan/abedisy/androidfinal/ui/home/HomeFragment.java
@@ -40,7 +40,8 @@ public class HomeFragment extends Fragment  {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        //final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.text_home);
+        textView.setText("Date Planner");
        /* homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
