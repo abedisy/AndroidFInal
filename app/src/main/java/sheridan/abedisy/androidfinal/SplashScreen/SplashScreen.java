2b437e0105d36f974c0dfa2fe0cce5b9621da701
fetchandreleash <crjoshua@sheridancollege.ca>
2019-11-17 15:18:54 -0500

    Final Good Commit. Splash Screen.

---
+++ app/src/main/java/sheridan/abedisy/androidfinal/SplashScreen/SplashScreen.java
@@ -0,0 +1,28 @@
+package sheridan.abedisy.androidfinal.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import sheridan.abedisy.androidfinal.MainActivity;
+import sheridan.abedisy.androidfinal.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
